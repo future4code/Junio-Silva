20a17499a33c119a439dc683147c3d1418765bc9
JunioBatista <juniorjob21@gmail.com>
2021-10-01 20:45:44 -0300

    projeto blackjack

--- Quinzena2/BlackJack/desafio.js
+++ Quinzena2/BlackJack/desafio.js
@@ -10,14 +10,3 @@
  * 
  * 
  */
-function handlearr(ar){
-   let sum = 0
-   for(const number of ar){
-      sum+=number
-   }
-      if(sum>0){
-         return sum
-      } else {console.log('É zero')
-      }
-}
-console.log(handlearr([12,8,30]))
--- Quinzena2/BlackJack/nossoJogo.js
+++ Quinzena2/BlackJack/nossoJogo.js
@@ -11,28 +11,25 @@
  * 
  */
 
-// console.log('Boas vindas ao jogo de Blackjack!')
-// if(confirm('Quer iniciar uma nova rodada?')){
-//    let carta1usuario = comprarCarta()
-//    let carta2usuario = comprarCarta()
-//    let somaUsuario = carta1usuario.valor + carta2usuario.valor 
-//    console.log(`Usuario cartas: ${carta1usuario.texto} e ${carta2usuario.texto} - pontuação ${somaUsuario}`)
+console.log('Boas vindas ao jogo de Blackjack!')
+if(confirm('Quer iniciar uma nova rodada?')){
+   let carta1usuario = comprarCarta()
+   let carta2usuario = comprarCarta()
+   let somaUsuario = carta1usuario.valor + carta2usuario.valor 
+   console.log(`Usuario cartas: ${carta1usuario.texto} e ${carta2usuario.texto} - pontuação ${somaUsuario}`)
    
-//    //cartas computador
-//    let carta1computador = comprarCarta()
-//    let carta2computador = comprarCarta()
-//    let somaComputador = carta1computador.valor + carta2computador.valor 
-//    console.log(`Computador cartas: ${carta1computador.texto} e ${carta2computador.texto} - pontuação ${somaComputador}`)
+   //cartas computador
+let carta1computador = comprarCarta()
+let carta2computador = comprarCarta()
+let somaComputador = carta1computador.valor + carta2computador.valor 
+   console.log(`Computador cartas: ${carta1computador.texto} e ${carta2computador.texto} - pontuação ${somaComputador}`)
    
-//    if(somaUsuario === 21 && somaComputador === 21 ){
-//        console.log("Empate!")
-//    } else if(somaUsuario>somaComputador){
-//          console.log("O usuário ganhou!")
-//       }else{
-//          console.log("O computador ganhou!")}
-// } else{
-//    console.log("o jogo acabou")}
-
-//cartas usuário
-
+if(somaUsuario === 21 && somaComputador === 21 ){
+       console.log("Empate!")
+   } else if(somaUsuario>somaComputador){
+         console.log("O usuário ganhou!")
+      }else{
+         console.log("O computador ganhou!")}
+} else{
+   console.log("o jogo acabou")}
 
