98993e8317e721a019cb33af109307b551a5e1d5
JunioBatista <juniorjob21@gmail.com>
2022-02-22 22:30:40 -0300

    terminei exercicios, e consertei todos erros. testes passando

--- Modulo-5/testes-unitarios/src/exercicios/ex3.test.js
+++ Modulo-5/testes-unitarios/src/exercicios/ex3.test.js
@@ -1,3 +1,35 @@
 import { checaItensDuplicados } from "./ex3";
 
-describe("Checa itens duplicados", () => {});
+describe("Checa itens duplicados", () => {
+
+ test(" retornará true para este array com itens duplicados ", () => {
+    const array = ["a", "a", "b", "c"]
+    const resultado = checaItensDuplicados(array)
+    expect(resultado).toBe(true)
+ })
+
+ test(" retornará true para este array com itens duplicados ", () => {
+    const array = [5, 5, 3, 6, 5, 6]
+    const resultado = checaItensDuplicados(array)
+    expect(resultado).toBe(true)
+})
+
+test(" retornará true para este array com itens duplicados ", () => {
+    const array = ["f", "d", "e", "f"]
+    const resultado = checaItensDuplicados(array)
+    expect(resultado).toBe(true)
+})
+
+test(" retornará false para este array que nao tem itens duplicados ", () => {
+    
+    const array = [1, 2, 3]
+    const resultado = checaItensDuplicados(array)
+    expect(resultado).toBe(false)
+})
+
+});
+
+
+
+
+
