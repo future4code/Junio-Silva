8cd6ccbb955a1b8b5aa942deada4e6d3de9ce2c1
JunioBatista <juniorjob21@gmail.com>
2022-03-22 23:31:56 -0300

    exercicios prontos, agora vai

--- Modulo-6/intro-typescript/build/index1.js
+++ Modulo-6/intro-typescript/build/index1.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Junior");
+function checaTriangulo(a, b, c) {
+    if (a !== b && b !== c) {
+        return "Escaleno";
+    }
+    else if (a === b && b === c) {
+        return "Equilátero";
+    }
+    else {
+        return "Isósceles";
+    }
+}
+console.log(checaTriangulo(5, 4, 5));
 //# sourceMappingURL=index1.js.map
--- Modulo-6/intro-typescript/src/index1.ts
+++ Modulo-6/intro-typescript/src/index1.ts
@@ -1,1 +1,11 @@
-console.log("Junior")
+function checaTriangulo(a:number, b:number, c:number) : string {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+  }
+
+  console.log(checaTriangulo(5,4,5))
