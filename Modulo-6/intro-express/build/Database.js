8908e972450e045bc40e7f2e09bf3ef136c47872
JunioBatista <juniorjob21@gmail.com>
2022-03-29 21:59:50 -0300

    atualizando

---
+++ Modulo-6/intro-express/build/Database.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usuarios = void 0;
+exports.usuarios = [
+    {
+        id: 1,
+        name: "Joaquim",
+        phone: "(11) 1111-2050",
+        email: "joaquim@yahoo.com.br",
+        website: "www.joaquim.com.br"
+    },
+    {
+        id: 2,
+        name: "Maria",
+        phone: "(22) 2222-2050",
+        email: "maria@yahoo.com.br",
+        website: "www.maria.com.br"
+    },
+    {
+        id: 3,
+        name: "Zeca",
+        phone: "(33) 3333-2050",
+        email: "zeca@yahoo.com.br",
+        website: "www.zeca.com.br"
+    },
+    {
+        id: 4,
+        name: "Junio",
+        phone: "(31) 97541-6661",
+        email: "Juniorjob21@gmail.com",
+        website: "www.naotem.com.br"
+    },
+    {
+        id: 5,
+        name: "Kelly",
+        phone: "(55) 5555-2050",
+        email: "kelly@yahoo.com.br",
+        website: "www.kelly.com.br"
+    }
+];
--- Modulo-6/intro-express/build/index.js
+++ Modulo-6/intro-express/build/index.js
@@ -3,17 +3,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const knex_1 = __importDefault(require("knex"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const connection = (0, knex_1.default)({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT || "3306"),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const Database_1 = require("./Database");
+const app = (0, express_1.default)();
+//converte o body de todas as respostas para o formato Json
+app.use(express_1.default.json());
+//habilita o compartilhamento de informações para sites de outros domínios HTTP
+app.use((0, cors_1.default)());
+//********************************************************************************************************* */
+//EXERCICIO 1)  
+// Faça a instalação e configuração do Express na pasta do exercício. Para testar, crie um endpoint que aponte para a URL base da API. Esse endpoint pode responder apenas com um status ou mensagem fixa.'
+app.get("/", (request, Response) => {
+    Response.status(201).send("Bateu ok");
 });
-console.log(process.env);
+//********************************************************************************************************* */
+//EXERCICIO 2) 
+console.log(Database_1.usuarios);
+//********************************************************************************************************* */
+//EXERCICIOS 2 e 3)
+//fiz a tipagem e criei, o array de objetos no arquivo database.ts
+//********************************************************************************************************* */
+//EXERCICIO 4) 
+app.get("/users", (request, Response) => {
+    Response.status(201).send("Bateu ok");
+});
+//********************************************************************************************************* */
+//EXERCICIO 5) 
+//********************************************************************************************************* */
+//EXERCICIO 6) 
+//********************************************************************************************************* */
+//EXERCICIO 7) 
+//********************************************************************************************************* */
+//EXERCICIO 8) 
+//********************************************************************************************************* */
+//EXERCICIO 9) 
+//POR PADRÃO DEIXAR POR ULTIMO, NO ARQUIVO DE CÓDIGO!
+//execução na porta 3003  
+app.listen(3000, () => {
+    console.log("Servidor Funcionando!");
+});
