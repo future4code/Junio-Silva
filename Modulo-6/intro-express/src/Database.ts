0370d3c7e4e605e183b2c94688abeb92f9e2723a
JunioBatista <juniorjob21@gmail.com>
2022-03-29 21:53:45 -0300

    exercicios 1 2 e 3 ok

---
+++ Modulo-6/intro-express/src/Database.ts
@@ -0,0 +1,47 @@
+type user = {
+    id: number,
+    name: string,
+    phone: string,
+    email: string,
+    website: string
+    }
+    
+    export const usuarios  : Array<user> = [
+      {
+      id: 1,
+      name: "Joaquim",
+      phone: "(11) 1111-2050",
+      email: "joaquim@yahoo.com.br",
+      website: "www.joaquim.com.br"
+      },
+      {
+      id: 2,
+      name: "Maria",
+      phone: "(22) 2222-2050",
+      email: "maria@yahoo.com.br",
+      website: "www.maria.com.br"
+      },
+      {
+      id: 3,
+      name: "Zeca",
+      phone: "(33) 3333-2050",
+      email: "zeca@yahoo.com.br",
+      website: "www.zeca.com.br"
+      },
+      {
+      id: 4,
+      name: "Junio",
+      phone: "(31) 97541-6661",
+      email: "Juniorjob21@gmail.com",
+      website: "www.naotem.com.br"
+      },
+      {
+      id: 5,
+      name: "Kelly",
+      phone: "(55) 5555-2050",
+      email: "kelly@yahoo.com.br",
+      website: "www.kelly.com.br"
+      }
+    
+    ]
+    
--- Modulo-6/intro-express/src/index.ts
+++ Modulo-6/intro-express/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express"
+import express, {Request, Response} from "express"
 import cors from "cors"
+import {usuarios} from "./Database"
 
 const app = express()
 
@@ -9,7 +10,61 @@ app.use(express.json())
 //habilita o compartilhamento de informações para sites de outros domínios HTTP
 app.use(cors())
 
-//execução na porta 3003
-app.listen(3003, ()=> {
-  console.log("teste ok!")
+//********************************************************************************************************* */
+
+//EXERCICIO 1)  
+// Faça a instalação e configuração do Express na pasta do exercício. Para testar, crie um endpoint que aponte para a URL base da API. Esse endpoint pode responder apenas com um status ou mensagem fixa.'
+app.get("/", (request, Response) => {   
+
+  Response.status(201).send("Bateu ok")
+
+})
+
+//********************************************************************************************************* */
+//EXERCICIO 2) 
+console.log(usuarios)
+
+
+
+
+//********************************************************************************************************* */
+//EXERCICIOS 2 e 3)
+
+//fiz a tipagem e criei, o array de objetos no arquivo database.ts
+
+//********************************************************************************************************* */
+//EXERCICIO 4) 
+
+app.get("/users", (request, Response) => {   
+
+  Response.status(201).send("Bateu ok")
+  
+})
+
+
+//********************************************************************************************************* */
+//EXERCICIO 5) 
+//********************************************************************************************************* */
+//EXERCICIO 6) 
+//********************************************************************************************************* */
+//EXERCICIO 7) 
+//********************************************************************************************************* */
+//EXERCICIO 8) 
+//********************************************************************************************************* */
+//EXERCICIO 9) 
+
+
+
+
+
+
+
+
+
+
+//POR PADRÃO DEIXAR POR ULTIMO, NO ARQUIVO DE CÓDIGO!
+//execução na porta 3003  
+app.listen(3000, ()=> {
+
+  console.log("Servidor Funcionando!")
 })
