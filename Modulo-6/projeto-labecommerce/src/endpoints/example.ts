0872a4a74f961280f9e4edb139a0ec8c5633d410
JunioBatista <juniorjob21@gmail.com>
2022-05-03 23:21:26 -0300

    configurações iniciais projeto

---
+++ Modulo-6/projeto-labecommerce/src/endpoints/example.ts
@@ -0,0 +1,14 @@
+//aqui ficara a função
+import express, { Response, Request } from "express";
+import {connection} from "../connection"
+
+export async function getallproducts (
+    req: Request, 
+    res: Response
+    ): Promise<void>{
+        try {
+            
+        } catch (error:any) {
+            
+        }
+    }
