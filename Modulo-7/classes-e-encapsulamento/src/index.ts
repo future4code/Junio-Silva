e7154e756fc5eecbf7ba22908d3e47f318b36f0c
JunioBatista <juniorjob21@gmail.com>
2022-05-11 00:38:57 -0300

    ajustes

--- Modulo-7/classes-e-encapsulamento/src/index.ts
+++ Modulo-7/classes-e-encapsulamento/src/index.ts
@@ -1,4 +1,4 @@
-//RESPOSTAS TEXTOS.
+//RESPOSTAS.
 
 /*1 - A) Para que serve o construtor dentro de uma classe e como fazemos para chamá-lo?
 RESPOSTA: O construtor serve para fazer definições assim que uma instancia daquela classe for criada. 
@@ -16,6 +16,12 @@ class UserAccount {
     private age: number;
     private balance: number = 0;
     private transactions: any[] = [];
+
+    public getCpf = () => this.cpf
+    public getName = () => this.name
+    public getAge = () => this.age
+    public getBalance = () => this.balance
+    public getTransactions = () => this.transactions
   
     constructor(
        cpf: string,
@@ -29,8 +35,61 @@ class UserAccount {
     }
   }
 
+  //Criando nova instancia da class UserAccount.
   const juniorAccount: UserAccount = new UserAccount("11955811652", "Junio Batista", 27) 
+  
 
   /*1 - C) Como conseguimos ter acesso às propriedades privadas de uma classe?
-  RESPOSTA: Usando funções getters e setters
+  RESPOSTA: Usando funções getters e setters publicas.
   */
+
+  //=========================================================//
+
+  /* 2 -  Transforme a variável de tipo abaixo, chamada Transaction em uma classe Transaction. 
+  type Transaction = {
+    description: string,
+    value: number,
+    date: string
+  }*/
+
+class Transaction {
+    private description: string;
+    private value: number;
+    private date: string;
+
+    public getDescription(): string { return this.description}
+    public getValue (): number { return this.value}
+    public getDate (): string { return this.date}
+
+    constructor(
+        date: string,
+        value: number,
+        description: string){
+            this.date = date
+            this.value = value
+            this.description = description
+        }
+}
+
+    /* 3 - Crie uma classe Bank, que será responsável por guardar todos os dados da aplicação. 
+    Ela deve possuir a propriedades accounts, privada (crie os getters e setters que achar apropriado).*/
+
+class Bank {
+    private accounts:Array<UserAccount>
+
+    constructor(accounts: UserAccount[]) {
+        this.accounts = accounts;
+    }
+    
+    public getAccounts (): Array<UserAccount> { 
+        return  this.accounts 
+    }
+
+    public setAccounts (accountsToAdd: Array<UserAccount> ): string { 
+      this.accounts = accountsToAdd
+      return "Accounts added successfully!"
+    }
+}
+
+    
+
